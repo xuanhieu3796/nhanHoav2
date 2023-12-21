5e3f656b3860156c33746f47d65186d05457716e
xuanhieu3796 <xuanhieu3796@gmail.com>
2023-12-21 09:46:21 +0700

    .

--- js/page.js
+++ js/page.js
@@ -26,4 +26,58 @@ $(document).ready(function() {
             }
         }
     });
+
+    new Swiper('.swiper_email_server', {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: "auto",
+        paginationClickable: true,
+        spaceBetween: 20,
+        breakpoints: {
+            1028: {
+                slidesPerView: "auto",
+                spaceBetween: 30
+            },
+            900: {
+                slidesPerView: "auto",
+                spaceBetween: 20
+            },
+            600: {
+                slidesPerView: "auto",
+                spaceBetween: 20
+            }
+        }
+    });
+
+    new Swiper('.swiper_sevice_02', {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 2,
+        paginationClickable: true,
+        spaceBetween: 20,
+        grid:{
+            fill:"row",
+            rows: 2
+        },
+        breakpoints: {
+            1028: {
+                slidesPerView: 5,
+                spaceBetween: 30
+            },
+            900: {
+                slidesPerView: 4,
+                spaceBetween: 20
+            },
+            600: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    });
 });
+
+
